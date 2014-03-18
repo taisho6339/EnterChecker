bcf913d9adc737fa4d2aa3717e8fcca8071b6352
Hiroki Sakamoto <taisho6339@Hiroki-no-MacBook-Pro.local>
2014-03-19 06:11:41 +0900

    add:Search Place Layout

---
+++ EnterChecker/src/com/taishonet/enter/chercheker/SearchPlaceActivity.java
@@ -0,0 +1,40 @@
+package com.taishonet.enter.chercheker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class SearchPlaceActivity extends Activity {
+
+	private Button mSearchButton;
+	private Button mAddButton;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.search_place_layout);
+		OnClickListener listener = new ButtonClickListener();
+		mSearchButton = (Button) findViewById(R.id.search_button);
+		mAddButton = (Button) findViewById(R.id.search_register_button);
+		mSearchButton.setOnClickListener(listener);
+		mAddButton.setOnClickListener(listener);
+	}
+
+	class ButtonClickListener implements OnClickListener {
+		@Override
+		public void onClick(View v) {
+			int id = v.getId();
+			switch (id) {
+			case R.id.search_button:
+				break;
+			case R.id.search_register_button:
+				break;
+			default:
+				break;
+			}
+		}
+	}
+
+}
